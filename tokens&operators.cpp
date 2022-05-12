3abdd17eb56a1c30ca13517c8f3752c3b412d949
catcodey <103652349+catcodey@users.noreply.github.com>
2022-05-12 19:49:12 +0530

    tokens&operators.cpp
     Size of int, float, double and char

---
+++ tokens&operators.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    float b;
+    char c;
+    double d;
+    cout<<"size of integer is "<<sizeof(a)<<"\n";
+    cout<<"size of float "<<sizeof(b)<<"\n";
+    cout<<"size of character "<<sizeof(c)<<"\n";
+    cout<<"size of double "<<sizeof(d);
+    return 0;
+}
